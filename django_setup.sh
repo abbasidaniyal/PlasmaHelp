f565828ac3fd9d97d65e8a28b16d2e825e48997b
Daniyal <abbasi.daniyal98@gmail.com>
2020-04-26 04:58:31 +0530

    fix: django_setup.sh

--- django_setup.sh
+++ django_setup.sh
@@ -5,5 +5,9 @@ sleep 60
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py shell -c "from users.models import User; User.objects.create_superuser('<ADMIN_EMAIL>', '<ADMIN_PASSWORD>')"
+python manage.py loaddata init_data.json
+
 python manage.py runserver 0.0.0.0:8000
 
