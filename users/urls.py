b82d8506f0902d4a464182afe5ea1bd7cc1e3597
Daniyal <abbasi.daniyal98@gmail.com>
2020-04-25 16:07:33 +0530

    add: Password change and reset

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 
-from users.views import *
+from users.views import (
+    home_page,
+    about_page,
+    LoginPageView,
+    LogoutPageView,
+    DonorRegisterView,
+    HospitalRegisterView,
+    ProfileView,
+    NearbyDonorView,
+    activate,
+)
+from django.contrib.auth import views as views
 
 urlpatterns = [
     path("", home_page, name="home-page"),
@@ -14,4 +25,28 @@ urlpatterns = [
     path("profile/", ProfileView.as_view(), name="profile"),
     path("hospital_dashboard", NearbyDonorView.as_view(), name="hospital-dashboard"),
     path("activate/<str:uidb64>/<str:token>/", activate, name="activate"),
+    path(
+        "password_change/", views.PasswordChangeView.as_view(), name="password_change"
+    ),
+    path(
+        "password_change/done/",
+        views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
