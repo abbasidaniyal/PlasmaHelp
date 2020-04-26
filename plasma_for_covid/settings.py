1a0a7d439743feb1d9952bfa5576ed3683b337d9
Daniyal Abbasi <abbasi.daniyal98@gmail.com>
2020-04-26 13:10:54 +0530

    Fixed Email Encode Error
    Email address was defined as a tuple rather than a string.

--- plasma_for_covid/settings.py
+++ plasma_for_covid/settings.py
@@ -114,11 +114,11 @@ AUTH_USER_MODEL = "users.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
@@ -151,6 +151,6 @@ MEDIA_URL = "/media/"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = (os.environ.get("EMAIL_ID", "foo@foo.com"),)
-EMAIL_HOST_PASSWORD = (os.environ.get("EMAIL_PASSWORD", "foo"),)
+EMAIL_HOST_USER = os.environ.get("EMAIL_ID", "foo@foo.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "foo")
 EMAIL_PORT = 587
