596d91d775cf84077d3ad3d70b378c34555f3476
Daniyal <abbasi.daniyal98@gmail.com>
2020-04-27 05:20:06 +0530

    fix: resend activation link and django to version 3.0.5

--- users/utils.py
+++ users/utils.py
@@ -1,23 +1,17 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.template.loader import render_to_string
-from django.utils import six
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk)
-            + six.text_type(timestamp)
-            + six.text_type(user.is_active)
-        )
+        return str(user.pk) + str(timestamp) + str(user.is_active)
 
 
 def send_mail(request, user):
     email_subject = "Activate Your Account"
     domain = request.get_host()
-    print(domain)
 
     message = render_to_string(
         "activate_account.html",
