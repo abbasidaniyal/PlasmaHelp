f68d15305c3dda2662fd795c068555c9ab2b35bd
Daniyal <abbasi.daniyal98@gmail.com>
2020-04-27 02:53:02 +0530

    fix: FAQ as static page

--- content/admin.py
+++ content/admin.py
@@ -3,4 +3,3 @@ from django.contrib import admin
 from content.models import *
 
 admin.site.register(Query)
-admin.site.register(FAQ)
--- content/urls.py
+++ content/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-from content.views import home_page, QueryPageView, FAQView
+from content.views import home_page, QueryPageView, faq_page
 
 urlpatterns = [
     path("", home_page, name="home-page"),
     path("query/", QueryPageView.as_view(), name="query"),
-    path("faqs/", FAQView.as_view(), name="faq"),
+    path("faqs/", faq_page, name="faq"),
 ]
