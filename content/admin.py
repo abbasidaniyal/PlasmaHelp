9b53fbfdbfcfa9b9bd6b039b20acb367b64a9e93
Daniyal Abbasi <daniyal.abbasi@innovaccer.com>
2020-09-02 13:45:07 +0530

    Service Termination Landing Page

--- content/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from content.models import *
-
-admin.site.register(Query)
--- profiles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from profiles.models import DonorProfile, HospitalProfile
-
-# Register your models here.
-admin.site.register(DonorProfile)
-admin.site.register(HospitalProfile)
